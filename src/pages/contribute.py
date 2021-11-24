9b4498d290c009973dd32e51c9b98b50fb5e95f7
William Elder <53864131+Welder90@users.noreply.github.com>
2021-11-24 14:42:43 +0000

    Update contribute.py

--- src/pages/contribute.py
+++ src/pages/contribute.py
@@ -4,4 +4,4 @@ import streamlit as st
 def main():
     st.title("Contribute")
     st.info("Feel free to contribute to this open source project. The github link can be found "
-                    "[here](https://github.com/Sayar1106/covid-dashboard)")
+                    "[here](https://github.com/Welder90/COVID)")
