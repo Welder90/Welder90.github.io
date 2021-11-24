37298991e6df7842b08472c12e63c861f8654ad5
William Elder <53864131+Welder90@users.noreply.github.com>
2021-11-24 14:31:15 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import src.pages.home
+import src.pages.data
+import src.pages.dashboard
+import src.pages.contribute
+import src.pages.about
+
+
+PAGES = {
+    "Home": src.pages.home,
+    "Data": src.pages.data,
+    "Dashboard": src.pages.dashboard,
+    "About": src.pages.about,
+    "Contribute": src.pages.contribute
+}
+
+def main():
+    st.sidebar.title("Menu")
+    choice = st.sidebar.radio("Navigate", list(PAGES.keys()))
+    PAGES[choice].main()
+    st.sidebar.title("About")
+    st.sidebar.info(
+        """
+        This app is maintained by Sayar Banerjee. You can learn more about me at
+        [sayar1106.github.io](https://Welder90.github.io).
+        """
+    )
+    st.sidebar.title("Contribute")
+    st.sidebar.info("Feel free to contribute to this open source project. The github link can be found "
+                    "[here](https://github.com/Welder90/COVID)")
+
+if __name__ == "__main__":
+    main()
